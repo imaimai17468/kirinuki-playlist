5d5151f3260cebc882754c168d8f98b489f13b3b
imaimai17468 <tosiki_801_life@outlook.jp>
2025-02-18 21:58:36 +0900

    Add new page routes for various app sections

---
+++ src/app/channels/page.tsx
@@ -0,0 +1,7 @@
+export default function Channels() {
+  return (
+    <div>
+      <h1>Channels</h1>
+    </div>
+  );
+}
---
+++ src/app/playlists/page.tsx
@@ -0,0 +1,7 @@
+export default function Playlists() {
+  return (
+    <div>
+      <h1>Playlists</h1>
+    </div>
+  );
+}
