c9a17d8ff32b57bfcc852a08643dace96af6080a
imaimai17468 <tosiki_801_life@outlook.jp>
2025-02-18 21:53:56 +0900

    Add dynamic AppBreadcrumb component for automatic path-based navigation

---
+++ src/components/parts/sidebar/breadcrumb.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { usePathname } from "next/navigation";
+
+export function AppBreadcrumb() {
+  const pathname = usePathname();
+  const breadcrumbItems = pathname
+    .split("/")
+    .slice(1)
+    .map((item) => ({
+      title: item,
+      url: `/${item}`,
+    }));
+
+  console.log(breadcrumbItems);
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {breadcrumbItems.map((item, index) => (
+          <BreadcrumbItem key={item.url}>
+            {index < breadcrumbItems.length - 1 ? (
+              <>
+                <BreadcrumbLink href={item.url}>{item.title}</BreadcrumbLink>
+                <BreadcrumbSeparator />
+              </>
+            ) : (
+              <BreadcrumbPage>{item.title}</BreadcrumbPage>
+            )}
+          </BreadcrumbItem>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
