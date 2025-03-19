af34d252513002c5223c79681c0a4295671dcff5
imaimai17468 <tosiki_801_life@outlook.jp>
2025-03-20 02:31:50 +0900

    chore: Add database configuration details including wrangler config path and database name to drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
+import type { Config } from "drizzle-kit";
 
 dotenv.config();
 
@@ -9,6 +9,8 @@ export default {
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
+    wranglerConfigPath: "wrangler.jsonc",
+    dbName: "kirinuki-playlist",
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID || "",
     databaseId: process.env.CLOUDFLARE_DATABASE_ID || "",
     token: process.env.CLOUDFLARE_D1_TOKEN || "",
