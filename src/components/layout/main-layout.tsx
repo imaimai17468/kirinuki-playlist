595d5a333ec1434e38c4d510dd63c9d696355e0f
imaimai17468 <tosiki_801_life@outlook.jp>
2025-03-25 17:59:02 +0900

    refactor: Update MainLayout component structure by adding ModeToggle and improving header layout for better responsiveness and organization

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,8 +1,8 @@
-import { Header } from "@/components/parts/header";
 import { AppSidebar } from "@/components/parts/sidebar/app-sidebar";
 import { AppBreadcrumb } from "@/components/parts/sidebar/navigation/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "../parts/mode-toggle";
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -14,16 +14,16 @@ export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <AppBreadcrumb />
+          <div className="flex justify-between w-full pr-4">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <AppBreadcrumb />
+            </div>
+            <ModeToggle />
           </div>
         </header>
-        <main className="px-4">
-          <Header />
-          {children}
-        </main>
+        <main className="px-4">{children}</main>
       </SidebarInset>
     </SidebarProvider>
   );
