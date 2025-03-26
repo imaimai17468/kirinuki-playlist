f51258309486e5fdfe9bdf16a8803e93be91b595
imaimai17468 <tosiki_801_life@outlook.jp>
2025-03-26 12:14:39 +0900

    refactor: Restructure MainLayout and Header components by removing unnecessary imports and consolidating layout elements for improved organization and readability

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,8 +1,6 @@
 import { AppSidebar } from "@/components/parts/sidebar/app-sidebar";
-import { AppBreadcrumb } from "@/components/parts/sidebar/navigation/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { ModeToggle } from "../parts/mode-toggle";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Header } from "../parts/header";
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -13,16 +11,7 @@ export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex justify-between w-full pr-4">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <AppBreadcrumb />
-            </div>
-            <ModeToggle />
-          </div>
-        </header>
+        <Header />
         <main className="px-4">{children}</main>
       </SidebarInset>
     </SidebarProvider>
--- src/components/parts/header.tsx
+++ src/components/parts/header.tsx
@@ -1,9 +1,19 @@
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import { ModeToggle } from "./mode-toggle";
+import { AppBreadcrumb } from "./sidebar/navigation/breadcrumb";
 
 export const Header: React.FC = () => {
   return (
-    <div className="absolute top-0 right-0 mr-8 mt-4 flex justify-end">
-      <ModeToggle />
-    </div>
+    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+      <div className="flex justify-between w-full pr-4">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <AppBreadcrumb />
+        </div>
+        <ModeToggle />
+      </div>
+    </header>
   );
 };
