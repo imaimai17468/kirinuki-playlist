f1e2fd0e71e35d6be6a0faaebde575e6d92c9ac5
imaimai17468 <tosiki_801_life@outlook.jp>
2025-03-29 23:49:35 +0900

    feat: youtube.tsに秒数を時間形式に変換する関数を追加

--- src/utils/youtube.ts
+++ src/utils/youtube.ts
@@ -10,3 +10,15 @@ export const formatDuration = (start: number, end: number): string => {
   const seconds = totalSeconds % 60;
   return `${minutes}:${seconds.toString().padStart(2, "0")}`;
 };
+
+export const convertSecondsToTimeFormat = (time: number): string => {
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = time % 60;
+
+  if (hours > 0) {
+    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  }
+
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+};
