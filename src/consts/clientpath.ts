a57e8549c0bbca74d3e92b0b0ac5ea0222261138
imaimai17468 <tosiki_801_life@outlook.jp>
2025-03-30 18:09:37 +0900

    feat: クライアントパスにプレイリスト詳細とプレイリストのルートを追加

--- src/consts/clientpath.ts
+++ src/consts/clientpath.ts
@@ -1,12 +1,14 @@
 const DETAIL_PATH = {
   CLIP_DETAIL: "/clips/:id",
   USERS_DETAIL: "/users/:id",
+  PLAYLIST_DETAIL: "/playlists/:id",
 } as const;
 
 export const CLIENT_PATH = {
   HOME: "/",
   CLIPS: "/clips",
   USERS: "/users",
+  PLAYLISTS: "/playlists",
   ...DETAIL_PATH,
 } as const;
 
